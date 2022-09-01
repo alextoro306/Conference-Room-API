d866b68233353151e6dadb7220c1251ce749f448
Juan Valenzuela <juan.valenzuela@tich.fi>
2022-09-01 16:51:01 +0300

    Added comments and a function
    Changes:
    •  Added comments and changed variable names for better readability
    •  Started to create a get API with a password for testing how to send object with node js

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import ical from 'node-ical';
 import cors from 'cors';
 import { type } from 'os';
+import { read } from 'fs';
+import { request } from 'http';
 
 /***
  * example link https://varia-plus.solenovo.fi:443/integration/dav/ROOM-1597752870-fi
@@ -10,6 +12,7 @@ import { type } from 'os';
  */
 
 
+/*INTERFACES | TYPES*/
 interface ICalReservation {
   type: string;
   params: [];
@@ -22,26 +25,53 @@ interface ICalReservation {
   end: string;
 }
 
- ical.async.fromURL('https://varia-plus.solenovo.fi:443/integration/dav/ROOM-1597752870-fi', {}, (err, data) => { 
+/*ON START*/
+ ical.async.fromURL('https://varia-plus.solenovo.fi:443/integration/dav/ROOM-1597752881-fi', {}, (err, data) => { 
   const d = JSON.stringify(data)
   const dd = JSON.parse(d) as {};
+  console.log(dd);
   const myData: ICalReservation[] = [];
 
   for (let [key, entry] of Object.entries(dd)) {
     const e = entry as ICalReservation;
     myData.push(e);
   }
-
-  console.log(myData.length)
-  myData.map(x => console.log(x.uid))
 });
+/*FUNCTIONS*/
+const getReservations = () : ICalReservation | null =>{
+  ical.async.fromURL('https://varia-plus.solenovo.fi:443/integration/dav/ROOM-1597752881-fi', {}, (err, data) =>{
+    const stringData = JSON.stringify(data);
+    const jsonData = JSON.parse(stringData) as {};
+    const reservations : ICalReservation[] = [];
 
+    for (let [key, entry] of Object.entries(jsonData)){
+      const reservtion = entry as ICalReservation;
+      reservations.push(reservtion);
+    }
+    return reservations;
+  });
+  return null;
+}
+
+
+
+/*CONFIGURATION*/
 const app = express();
 app.use(cors({ origin: '*' }));
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+/*GET*/
+app.get('/Reservations', (req, res) => {
+  const header = req.headers['password'];
+  console.log(header);
+  const  reservations = JSON.stringify(getReservations());
+  console.log(header);
+
+
+  res.send(reservations);
+});
 app.get('/hello', (req, res) => {
   res.send('Hello World!');
 });
@@ -49,7 +79,7 @@ app.get('/hello', (req, res) => {
 app.get('/moi', (req, res ) => {
   res.send('Hei Kaikki!');
 })
-
+/*POST*/
 app.post('/kakka', (req, res) => {
   const password = "kakka";
   const data:string = req.body.password;
@@ -72,7 +102,7 @@ app.post('/room', (req, res) => {
 
 
 })
-
+/*CONFIGURATION*/
 app.listen(5000, () => {
   console.log('Server running on port 5000');
 });
