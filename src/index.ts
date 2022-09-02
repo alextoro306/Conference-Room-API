877fe03dcad944e8ee621dd544408e0d4207db88
TichFi <admin@tich.fi>
2022-09-02 12:06:57 +0300

    Added a ".json" File to test local storage and created a new interface
    Change:
    •  new file "Reservations.json"
    •  new interface "Reservations"

--- src/index.ts
+++ src/index.ts
@@ -24,7 +24,17 @@ interface ICalReservation {
   uid: string;
   end: string;
 }
-
+interface Reservation {
+  type: string;
+  params: [];
+  dtstamp: string;
+  start: string;
+  datetype: string;
+  duration: {hours: number, minutes: number};
+  summary: string;
+  uid: string;
+  end: string;
+}
 /*FUNCTIONS*/
 const getReservationsAsync = async () =>{
   let reservations: ICalReservation[] = [];
@@ -32,6 +42,8 @@ const getReservationsAsync = async () =>{
     const cal = await ical.async.fromURL('https://varia-plus.solenovo.fi:443/integration/dav/ROOM-1597752881-fi');
     const stringReservations = await JSON.stringify(cal); 
     const jsonReservations = await JSON.parse(stringReservations);
+    console.log(jsonReservations);
+    /*convert object into ICalReservation and insert it into reservations array*/
     for await (let [key, entry] of Object.entries(jsonReservations)) {
       const e = entry as ICalReservation;
       reservations.push(e);
@@ -39,6 +51,7 @@ const getReservationsAsync = async () =>{
   }
   catch (err){
     console.log('Error: ' + err);
+    return null;
   }
   return await reservations;
 };
@@ -53,10 +66,23 @@ app.use(express.json());
 
 /*GET*/
 app.get('/reservations', async (req, res) => {
-  const header = req.headers['password'];
+  const token = req.headers['varia_reservations_token'];
+  const localToken = 'ani9_5ce_8p5_yfe3_nas1_!#d';
   const reservations = await getReservationsAsync();
-  res.send(reservations);
+  
+  if (!token) {
+    res.status(400).send("Invalid data")
+  }
+  if (token === localToken) {
+    res.status(200).send(reservations)
+  } else {
+    res.status(401).send("Incorrect Token")
+  }
 });
+
+
+
+
 app.get('/hello', (req, res) => {
   res.send('Hello World!');
 });
