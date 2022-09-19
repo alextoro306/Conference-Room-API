f3c284185a1db49fe2cc71d0ab7e0ebe2d826fea
JoniRinta-Kahila <joni.rintakahila@opp.eduvantaa.fi>
2022-09-19 13:17:53 +0300

    refactor

--- src/index.ts
+++ src/index.ts
@@ -1,114 +1,99 @@
 import express from 'express';
 import ical from 'node-ical';
 import cors from 'cors';
-import reservationsFile from './Reservations.json'
+import type { ICalReservation, IResponseData } from './types';
+import dataFromDB from './dataFromDB';
 
-/***
- * example link https://varia-plus.solenovo.fi:443/integration/dav/ROOM-1597752870-fi
- * room info: ROOM-1597752870-fi
- * link base: https://varia-plus.solenovo.fi:443/integration/dav/
- */
-
-
-/*INTERFACES | TYPES*/
-interface ICalReservation {
-  type: string;
-  params: [];
-  dtstamp: string;
-  start: string;
-  datetype: string;
-  duration: string;
-  summary: string;
-  uid: string;
-  end: string;
-}
-interface Reservation {
-  type: string;
-  params: [];
-  dtstamp: string;
-  start: string;
-  datetype: string;
-  duration: { hours: number, minutes: number };
-  summary: string;
-  uid: string;
-  end: string;
-}
-/*FUNCTIONS*/
-const getReservationsAsync = async (room:string) : Promise<ICalReservation[] | null> => {
-  let reservations: ICalReservation[] = [];
-  try {
-    const cal = await ical.async.fromURL('https://varia-plus.solenovo.fi:443/integration/dav/' + room);
-    const stringReservations = await JSON.stringify(cal);
-    const jsonReservations = await JSON.parse(stringReservations);
-    console.log(jsonReservations);
-    /*convert object into ICalReservation and insert it into reservations array*/
-    for await (let [key, entry] of Object.entries(jsonReservations)) {
-      const e = entry as ICalReservation;
-      reservations.push(e);
-    }
-  }
-  catch (err) {
-    console.log('Error: ' + err);
-    return null;
-  }
-  return await reservations;
-};
-/*Alex fix this*/
-const getReservations = () => {
-  let reservations: ICalReservation[] = [];
-  try {
-    // const stringReservations = fs.readFileSync('Reservations.json', 'utf-8');
-    // const stringReservations = await JSON.stringify(cal);
-    const jsonReservations = JSON.parse(JSON.stringify(reservationsFile));
-    console.log(jsonReservations);
-    /*convert object into ICalReservation and insert it into reservations array*/
-    for (let [key, entry] of Object.entries(jsonReservations)) {
-      const e = entry as ICalReservation;
-      reservations.push(e);
-    }
-  } catch (error) {
-    console.log('Error: ' + error)
-  }
-};
-
-
-/*CONFIGURATION*/
+/* CONFIGURATION */
 const app = express();
 app.use(cors({ origin: '*' }));
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+/* FUNCTIONS */
+const getReservationsAsync = async (
+  room: string
+): Promise<ICalReservation[] | null> => {
+  const reservations: ICalReservation[] = [];
+  try {
+    const cal = await ical.async.fromURL(
+      `https://varia-plus.solenovo.fi:443/integration/dav/${room}`
+    );
+    const stringReservations = JSON.stringify(cal);
+    const jsonReservations = JSON.parse(stringReservations);
+    console.log(jsonReservations);
 
-/*GET*/
+    /* convert object into ICalReservation and insert it into reservations array */
+    // eslint-disable-next-line no-restricted-syntax, no-unused-vars
+    for (const [key, entry] of Object.entries(jsonReservations)) {
+      const e = entry as ICalReservation;
+      reservations.push(e);
+    }
+  } catch (err) {
+    console.log('Error:', err);
+    return null;
+  }
+  return reservations;
+};
+
+/* Alex fix this */
+// const getReservations = () => {
+//   let reservations: ICalReservation[] = [];
+//   try {
+//     // const stringReservations = fs.readFileSync('Reservations.json', 'utf-8');
+//     // const stringReservations = await JSON.stringify(cal);
+//     const jsonReservations = JSON.parse(JSON.stringify(reservationsFile));
+//     console.log(jsonReservations);
+//     /*convert object into ICalReservation and insert it into reservations array*/
+//     for (let [key, entry] of Object.entries(jsonReservations)) {
+//       const e = entry as ICalReservation;
+//       reservations.push(e);
+//     }
+//   } catch (error) {
+//     console.log('Error: ' + error)
+//   }
+// };
+
+/* GET */
 app.post('/reservations', async (req, res) => {
   const room: string = req.body.room_id;
 
   if (!room) {
-    res.status(400).json({error: 'Room info'})
+    return res.status(400).json({ error: 'Room info' });
   }
 
+  const roomName =
+    dataFromDB.find((x) => x.roomId === room)?.roomName ??
+    'Huoneen nimi puuttu';
+
   const reservations = await getReservationsAsync(room);
-  res.status(200).send(reservations);
+
+  if (!reservations)
+    return res.status(404).json({
+      err: 'no data found',
+    });
+
+  const responseData: IResponseData = {
+    roomId: room,
+    roomName,
+    reservations: [],
+  };
+  return res.status(200).json(responseData);
 });
 
 app.post('/room', (req, res) => {
-  const roomnumber = req.body.roomnumber
+  // const roomnumber = req.body.roomnumber
 
-  if (!roomnumber) {
-    res.status(400).send("Invalid data")
-  }
-})
+  // if (!roomnumber) {
+  //   res.status(400).send("Invalid data")
+  // }
+  res.send('🚧');
+});
 
-
-/*ON START*/
-console.log(getReservations());
-
-
-/*CONFIGURATION*/
+/* CONFIGURATION */
 app.listen(5000, () => {
   console.log('Server running on port 5000');
 });
 
 export default app;
-
